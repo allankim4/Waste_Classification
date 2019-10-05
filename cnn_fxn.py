df11af804ce2717570b4df1b65ad28fbb220c59d
allankim4 <miknalla@gmail.com>
2019-10-05 02:27:47 -0400

    model improvement

--- cnn_fxn.py
+++ cnn_fxn.py
@@ -6,16 +6,16 @@ import random
 import urllib
 
 #Loading 2-layer
-cnn = load_model('image_class1.h5')
+cnn = load_model('Trained_Model/image_class1.h5')
 
 #Loading InceptionV3
-cnn2 = load_model('image_class1_tl.h5')
+cnn2 = load_model('Trained_Model/image_class1_tl.h5')
 
 #Loading 3-layer
-cnn3 = load_model('image_class2_3l.h5')
+cnn3 = load_model('Trained_Model/image_class2_3l.h5')
 
 #Loading Xception
-cnn3_tl = load_model('image_class2_3l_tl.h5')
+cnn3_tl = load_model('Trained_Model/image_class2_3l_tl.h5')
 
 
 def downloader(image_url):
@@ -37,6 +37,7 @@ def image_predictor(path):
 
     return pred1, pred2, pred3, pred4
 
+
 def prediction_label(p):
     if p > .7:
         p = 'Organic'
@@ -49,21 +50,19 @@ def prediction_label(p):
 
 def predictor_output(path):
     mod1, mod2, mod3, mod4 = image_predictor(path)
-    predictions = [mod1, mod2, mod3, mod4]
-    labels = [pred1, pred2, pred3, pred4]
 
     print('VALUE Interpretation:')
     print()
     print('Closer to 1 is Organic, Closer to 0 is Recyclable')
     print()
     print('2-LAYER CNN:')
-    print(prediction_label(pred1))
+    print(prediction_label(mod1))
     print()
     print('3-Layer CNN:')
-    print(prediction_label(pred2))
+    print(prediction_label(mod2))
     print()
     print('Inception ver3 CNN')
-    print(prediction_label(pred3))
+    print(prediction_label(mod3))
     print()
     print('Xception CNN')
-    print(prediction_label(pred4))
+    print(prediction_label(mod4))
